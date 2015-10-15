0f422324180c39b80d8dfb98b18d9dabb1311ff0
Steve <steven.g.ball@gmail.com>
2015-10-15 09:20:17 -0400

    More WIP framework coding

---
+++ source/VoxGame.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+class VoxGame
+{
+public:
+	/* Public methods */
+
+protected:
+	/* Protected methods */
+
+private:
+	/* Private methods */
+
+public:
+	/* Public members */
+
+protected:
+	/* Protected members */
+
+private:
+	/* Private members */
+};
