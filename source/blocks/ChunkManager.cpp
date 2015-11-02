bc32b86b31e88865b7e42592b8ca50644e6414d0
Steve <steven.g.ball@gmail.com>
2015-11-01 20:09:34 -0500

    Add chunk manager (WIP), Add lua and selene to cmake.

---
+++ source/blocks/ChunkManager.cpp
@@ -0,0 +1,26 @@
+// ******************************************************************************
+//
+// Filename:	ChunkManager.cpp
+// Project:		Game
+// Author:		Steven Ball
+//
+// Purpose:
+//
+// Revision History:
+//   Initial Revision - 01/11/15
+//
+// Copyright (c) 2005-2015, Steven Ball
+//
+// ******************************************************************************
+
+#include "ChunkManager.h"
+
+
+ChunkManager::ChunkManager()
+{
+
+}
+
+ChunkManager::~ChunkManager()
+{
+}
---
+++ source/blocks/ChunkManager.h
@@ -0,0 +1,40 @@
+// ******************************************************************************
+//
+// Filename:	ChunkManager.h
+// Project:		Game
+// Author:		Steven Ball
+//
+// Purpose:
+//
+// Revision History:
+//   Initial Revision - 01/11/15
+//
+// Copyright (c) 2005-2015, Steven Ball
+//
+// ******************************************************************************
+
+#pragma once
+
+
+class ChunkManager
+{
+public:
+	/* Public methods */
+	ChunkManager();
+	~ChunkManager();
+
+protected:
+	/* Protected methods */
+
+private:
+	/* Private methods */
+
+public:
+	/* Public members */
+
+protected:
+	/* Protected members */
+
+private:
+	/* Private members */
+};
