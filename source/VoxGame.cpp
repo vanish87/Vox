d0e5e940df2c0a117996acad2fdfd2979782ba17
Steve <steven.g.ball@gmail.com>
2015-11-12 09:22:21 -0500

    Better lighting position, for shadow and light rendering

--- source/VoxGame.cpp
+++ source/VoxGame.cpp
@@ -81,7 +81,7 @@ void VoxGame::Create()
 	m_pRenderer->CreateFreeTypeFont("media/fonts/arial.ttf", 12, &m_defaultFont);
 
 	/* Create lights */
-	m_defaultLightPosition = vec3(3.0f, 5.0f, 3.0f);
+	m_defaultLightPosition = vec3(300.0f, 500.0f, 300.0f);
 	m_defaultLightView = vec3(0.0f, 0.0f, 0.0f);
 	vec3 lightDirection = m_defaultLightView - m_defaultLightPosition;
 	m_pRenderer->CreateLight(Colour(1.0f, 1.0f, 1.0f, 1.0f), Colour(1.0f, 1.0f, 1.0f, 1.0f), Colour(0.0f, 0.0f, 0.0f, 1.0f),
