777f456d129bfc0ba339bc8996b8356295e982bd
Steve <AlwaysGeeky@users.noreply.github.com>
2015-12-08 00:34:27 -0500

    Tabbing

--- source/VoxWindow.cpp
+++ source/VoxWindow.cpp
@@ -1,7 +1,7 @@
 // ******************************************************************************
 // Filename:	VoxWindow.cpp
-// Project:		Vox
-// Author:		Steven Ball
+// Project:	Vox
+// Author:	Steven Ball
 // 
 // Revision History:
 //   Initial Revision - 27/10/15
