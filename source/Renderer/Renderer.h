e689e4e8ce756b21b77ed8e20a735466b2f8a1e6
Steve <AlwaysGeeky@users.noreply.github.com>
2016-01-18 16:10:15 -0500

    file desc

--- source/Renderer/Renderer.h
+++ source/Renderer/Renderer.h
@@ -1,6 +1,6 @@
 // ******************************************************************************
 // Filename:    Renderer.h
-// Project:     Game
+// Project:     Vox
 // Author:      Steven Ball
 //
 // Purpose:
