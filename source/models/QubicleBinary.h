af847535c45bfe8abca363136c2fc71dbdbd0138
Steve <AlwaysGeeky@users.noreply.github.com>
2016-02-09 10:57:44 -0500

    Update QubicleBinary.h

--- source/models/QubicleBinary.h
+++ source/models/QubicleBinary.h
@@ -1,8 +1,7 @@
 // ******************************************************************************
-//
-// Filename:	QubicleBinary.h
-// Project:	Game
-// Author:	Steven Ball
+// Filename:    QubicleBinary.h
+// Project:     Game
+// Author:      Steven Ball
 //
 // Purpose:
 //
@@ -10,7 +9,6 @@
 //   Initial Revision - 10/07/14
 //
 // Copyright (c) 2005-2015, Steven Ball
-//
 // ******************************************************************************
 
 #pragma once
