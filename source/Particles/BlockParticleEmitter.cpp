4f9bc51d32519dd186f076b2939dd96cc1f992a9
Steve <AlwaysGeeky@users.noreply.github.com>
2016-02-18 14:46:11 -0500

    file desc

--- source/Particles/BlockParticleEmitter.cpp
+++ source/Particles/BlockParticleEmitter.cpp
@@ -1,16 +1,14 @@
 // ******************************************************************************
-//
-// Filename:	BlockParticleEmitter.cpp
-// Project:		Game
-// Author:		Steven Ball
+// Filename:    BlockParticleEmitter.cpp
+// Project:     Vox
+// Author:      Steven Ball
 //
 // Purpose:
 //
 // Revision History:
 //   Initial Revision - 09/11/14
 //
-// Copyright (c) 2005-2011, Steven Ball
-//
+// Copyright (c) 2005-2016, Steven Ball
 // ******************************************************************************
 
 #include "BlockParticleEmitter.h"
