7ddc7b73afe2ca92a8a4535deb9ddd92810fbc06
Steve <AlwaysGeeky@users.noreply.github.com>
2016-02-26 00:11:00 -0500

    file desc

--- source/Projectile/ProjectileManager.cpp
+++ source/Projectile/ProjectileManager.cpp
@@ -1,16 +1,14 @@
 // ******************************************************************************
-//
-// Filename:	ProjectileManager.cpp
-// Project:		Game
-// Author:		Steven Ball
+// Filename:    ProjectileManager.cpp
+// Project:     Vox
+// Author:      Steven Ball
 //
 // Purpose:
 //
 // Revision History:
 //   Initial Revision - 15/02/15
 //
-// Copyright (c) 2005-2011, Steven Ball
-//
+// Copyright (c) 2005-2016, Steven Ball
 // ******************************************************************************
 
 #include "ProjectileManager.h"
