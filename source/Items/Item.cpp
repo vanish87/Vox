538abc235083ad5229f1f45a5373ba95872d75a2
Steve <AlwaysGeeky@users.noreply.github.com>
2016-03-08 14:44:42 -0500

    Update Item.cpp

--- source/Items/Item.cpp
+++ source/Items/Item.cpp
@@ -1,6 +1,6 @@
 // ******************************************************************************
 // Filename:    Item.cpp
-// Project:     Game
+// Project:     Vox
 // Author:      Steven Ball
 //
 // Revision History:
