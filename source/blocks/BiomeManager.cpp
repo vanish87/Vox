e63778753957367f57e11dff14338bcd24b0a5d9
Steve <steven.g.ball@gmail.com>
2016-06-14 08:10:24 -0400

    Remove test code

--- source/blocks/BiomeManager.cpp
+++ source/blocks/BiomeManager.cpp
@@ -52,36 +52,6 @@ BiomeManager::BiomeManager(Renderer* pRenderer)
 	// Biome regions	
 	biomeRegions.SetFrequency(0.005f);
 	biomeRegions.SetSeed(2);
-
-	// TODO : Remove this test code below:
-	//utils::NoiseMap heightMap;
-	//utils::NoiseMapBuilderPlane heightMapBuilder;
-	//heightMapBuilder.SetSourceModule(biomeRegions);
-	//heightMapBuilder.SetDestNoiseMap(heightMap);
-	//heightMapBuilder.SetDestSize(256, 256);
-	//heightMapBuilder.SetBounds(6.0, 10.0, 1.0, 5.0);
-	//heightMapBuilder.Build();
-	//utils::RendererImage renderer;
-	//utils::Image image;
-	//renderer.SetSourceNoiseMap(heightMap);
-	//renderer.SetDestImage(image);
-	//renderer.ClearGradient();
-	//renderer.AddGradientPoint(-1.0000, utils::Color(0, 0, 128, 255)); // deeps
-	//renderer.AddGradientPoint(-0.2500, utils::Color(0, 0, 255, 255)); // shallow
-	//renderer.AddGradientPoint(0.0000, utils::Color(0, 128, 255, 255)); // shore
-	//renderer.AddGradientPoint(0.0625, utils::Color(240, 240, 64, 255)); // sand
-	//renderer.AddGradientPoint(0.1250, utils::Color(32, 160, 0, 255)); // grass
-	//renderer.AddGradientPoint(0.3750, utils::Color(224, 224, 0, 255)); // dirt
-	//renderer.AddGradientPoint(0.7500, utils::Color(128, 128, 128, 255)); // rock
-	//renderer.AddGradientPoint(1.0000, utils::Color(255, 255, 255, 255)); // snow
-	//renderer.EnableLight();
-	//renderer.SetLightContrast(3.0); // Triple the contrast
-	//renderer.SetLightBrightness(2.0); // Double the brightness
-	//renderer.Render();
-	//utils::WriterBMP writer;
-	//writer.SetSourceImage(image);
-	//writer.SetDestFilename("tutorial.bmp");
-	//writer.WriteDestFile();
 }
 
 BiomeManager::~BiomeManager()
